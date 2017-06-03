2c07932a85bf5608b9bdb98b251b2ce6ec385c36
Hack Reactor Student <student@hackreactor.com>
2017-06-02 20:30:03 -0700

    Working App, needs to be cleaned up

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,12 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
+var Search = (props) => (
+  <form onSubmit={ function(event) { event.preventDefault(); props.onNavSubmit(event.target.children[0].children[0].value, -1); }}>
+    <div className="search-bar form-inline">
+      <input className="form-control" type="text" onChange={ function(event) { props.onNavSubmit(event.target.value, props.step); }}/>
+      <button className="btn hidden-sm-down" >
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div> 
+  </form>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
